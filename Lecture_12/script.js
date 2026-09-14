8596337e8c9f8de0b55c330ee19b28e1e1e9cca0
AnanoGagnidze <anano.gagnidze906@eab.tsu.edu.ge>
2026-09-15 01:06:37 +0400

    added

--- Lecture_12/script.js
+++ Lecture_12/script.js
@@ -12,12 +12,52 @@ function checkUserAge() {
 
 function calculateLateFee() {
     let days = Number(document.getElementById("days").value);
-    let fee = "";
+    let fee = 0;
 
     if (days) {
-        fee = (days <=3) ? "You have no late fee" : "Your late fee is";
-        document.getElementById("fee").innerHTML = fee + " for" + days + " days";
+        if (days >=1 && days <=3) {
+            fee = days * 1;
+        } else if (days >=4 && days <=7) {
+            fee = days * 2;
+        } else {
+            fee = days * 5;
+        }
 
+        document.getElementById("fee").innerHTML = "Your fee is " + fee + "$";
+
+    }    
+}
+
+function calculateElectricityBill() {
+    let units = Number(document.getElementById("units").value);
+    let bill = 0;
+
+    if (units) {
+        if (units >=1 && units <=100) {
+            bill = units * 0.5;
+        } else if (units >=101 && units <=200) {
+            bill = units * 0.75;
+        } else  {
+            bill = units * 1;
+        }
+
+        document.getElementById("bill").innerHTML = "Your Electricity Bill is " + bill + "$";
     }
-    
+}
+
+function applyDiscount() {
+    let order = Number(document.getElementById("order").value)
+    let discount = 0
+
+    if (order) {
+        if (order >= 100 && order <= 200) {
+            discount = order * 0.1;
+        } else if (order > 200) {
+            discount = order * 0.15;
+        } else {
+            discount = 0;
+        }
+
+        document.getElementById("discount").innerHTML = "Your Discount is " + discount + "$";
+    }
 }
